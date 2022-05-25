97fe27314930885587725929632827051bcb41b8
E-Dawkins <ethandawkins@hotmail.co.uk>
2022-05-25 16:31:15 +1000

    Initial Commit

---
+++ projects/CMake-HelloWorld/src/CMake-HelloWorld.cpp
@@ -0,0 +1,12 @@
+﻿// CMake-HelloWorld.cpp : Defines the entry point for the application.
+//
+
+#include "CMake-HelloWorld.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
